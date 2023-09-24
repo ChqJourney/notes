1aadb4aa31fe5e889b3b7839830eaf47c2c1dc38
ChqJourney <20310381@qq.com>
2023-09-25 07:31:57 +0800

    dd

--- note_api/src/main.rs
+++ note_api/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::SocketAddr};
+
+use axum::{Router, extract::Path, routing::get, response::IntoResponse, http::StatusCode, Json};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_error_handling_and_dependency_injection=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/users/:id", get(users_show))
+        .layer(TraceLayer::new_for_http());
+
+    // Run our application
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn users_show()->impl IntoResponse{
+    (StatusCode::OK, Json({}))
 }
