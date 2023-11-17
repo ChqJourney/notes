a19d4ebb7f96904dfe461dfe10869d09bf9ae273
parick <20310381@qq.com>
2023-11-17 22:55:29 +0800

    dd

---
+++ web_ui/src/lib/nav/auth.svelte
@@ -0,0 +1,16 @@
+<script>
+
+    let isAuthenticated=false;
+    let userInfo=undefined;
+</script>
+
+<div>
+    {#if isAuthenticated}
+        <a href="#">{userInfo?.email??"error"}</a>
+    {:else}
+    <div class="flex gap-2">
+        <a href="#" class="hover:bg-slate-400 hover:text-lime-300 hover:scale-105 px-2 py-1 rounded-md">Register</a>
+        <a href="#" class="hover:bg-slate-400 hover:text-lime-300 hover:scale-105 px-2 py-1 rounded-md">Login</a>
+    </div>
+    {/if}
+</div>
