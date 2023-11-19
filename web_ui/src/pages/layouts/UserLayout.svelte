bc3501b1c9a28d4fe9fa86322410a7a6a2e1a251
ChqJourney <20310381@qq.com>
2023-11-19 22:39:40 +0800

    dd

---
+++ web_ui/src/pages/layouts/UserLayout.svelte
@@ -0,0 +1,20 @@
+<script>
+    // @ts-nocheck
+
+    import { Route } from "svelte-router-spa";
+    import logo from "../../assets/svelte.svg";
+    import { userStore } from "../../stores/userStore";
+    import Auth from "../../lib/nav/auth.svelte";
+    export let currentRoute;
+
+    const params = {};
+</script>
+
+<div class="flex justify-between">
+    <img width="40" src={logo} alt="logo" />
+    <Auth />
+</div>
+<hr />
+<main>
+    <Route {currentRoute} {params} />
+</main>
